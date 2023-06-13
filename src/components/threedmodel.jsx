b5c30790c039c9ef20a4cd720d2c42ee4b2b7f29
dKurtalic <dkurtalic1@etf.unsa.ba>
2023-06-14 00:42:31 +0200

    3d model update

--- src/components/threedmodel.jsx
+++ src/components/threedmodel.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import ReactModel from '../components/ReactModel';
+import { OrbitControls, Stage } from '@react-three/drei';
+import SpotifyModel from '../components/SpotifyModel';
 
 const Threedmodel = () => {
   return (
@@ -10,15 +10,16 @@ const Threedmodel = () => {
         display: 'inline-block',
         alignItems: 'center',
         justifyContent: 'center',
-        justifyContent: 'center',
         marginTop: '3rem'
       }}
     >
 
 
       <Canvas style={{ width: '400px', height: '400px' }}>
+        <Stage environment="city" intensity={0.5}>
+          <SpotifyModel position={[0, 0, 0]} scale={[1.5, 1.5, 1.5]} />
+        </Stage>
         <OrbitControls enableZoom={false} />
-        <ReactModel position={[0, 0, 0]} scale={[1.2, 1.2, 1.2]} />
       </Canvas>
     </div>
   );
