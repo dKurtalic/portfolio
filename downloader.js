15c12f81d453f855af818d03ad1283e94b9bd4d1
dKurtalic <dkurtalic1@etf.unsa.ba>
2023-06-20 19:53:14 +0200

    Bug fixed

--- downloader.js
+++ downloader.js
@@ -1,21 +1,17 @@
 import { React } from "react";
 
 
-
 export default async function downloadFile(url) {
+    const filePath = './src/assets/PismoPreporuke_DženanaĐonko.pdf';
 
-
-
-    fetch(url)
-        .then((response) => response.blob())
-        .then((blob) => {
-            const a = document.createElement('a');
-            a.href = URL.createObjectURL(blob);
-            a.download = 'RecommendationLetter_DinaKurtalic.pdf';
-            a.click();
-        })
-        .catch((error) => {
-            console.error('Download error:', error);
-        });
+    try {
+        const response = await fetch(filePath);
+        const blob = await response.blob();
+        const a = document.createElement('a');
+        a.href = URL.createObjectURL(blob);
+        a.download = 'RecommendationLetter_DinaKurtalic.pdf';
+        a.click();
+    } catch (error) {
+        console.error('Download error:', error);
+    }
 }
-
